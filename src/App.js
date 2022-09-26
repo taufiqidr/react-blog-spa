bca61e279ec561b85f0eca6e86cc8a671cf5a60b
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2022-09-26 09:22:39 +0800

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home';
+import NewPost from './NewPost';
+
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import Layout from './Layout';
+import EditPost from './EditPost';
+
+import { Route, Routes } from 'react-router-dom';
+import useAxiosFetch from './hooks/useAxiosFetch';
+import { useEffect } from 'react';
+import { useStoreActions } from 'easy-peasy'
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts)
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
+
+  useEffect(() => {
+    setPosts(data)
+  }, [data, setPosts])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home isLoading={isLoading} fetchError={fetchError} />} />
+        <Route path='post'>
+          <Route index element={<NewPost />} />
+          <Route path=':id' element={<PostPage />} />
+        </Route>
+        <Route path='edit/:id' element={<EditPost />} />
+        <Route path='about' element={<About />} />
+        <Route path='*' element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
